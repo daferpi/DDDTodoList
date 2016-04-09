bb695a77d00efeccce55663939f6242607f82c22
daferpi <daferpi@gmail.com>
2016-04-09 22:15:38 +0200

    commit inicial

---
+++ src/main/java/infraestructure/Application.java
@@ -0,0 +1,15 @@
+package infraestructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by daferpi on 09/04/16.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String []args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
