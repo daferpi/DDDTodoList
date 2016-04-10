bf8b7d4c8836931ed632352e58c738ad50e96635
daferpi <daferpi@gmail.com>
2016-04-10 08:18:39 +0200

    se modifica la configuracion para poder usar JPA y REST simultaneamente

--- src/main/java/infraestructure/Application.java
+++ src/main/java/infraestructure/Application.java
@@ -2,8 +2,8 @@ package infraestructure;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
 
 import javax.annotation.Resource;
 
@@ -11,12 +11,8 @@ import javax.annotation.Resource;
  * Created by daferpi on 09/04/16.
  */
 @SpringBootApplication
-public class Application extends WebSecurityConfigurerAdapter {
+public class Application {
 
-    @Resource
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("test").password("test").roles("USER");
-    }
 
     public static void main(String []args) {
         SpringApplication.run(Application.class,args);
